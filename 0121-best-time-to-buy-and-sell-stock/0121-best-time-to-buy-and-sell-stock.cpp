bd2fa7dd5f6d8fba2e6d2fc2caf333fa58f4a403
JaeHo Jeong <124887853+Jaero0@users.noreply.github.com>
2026-04-30 11:26:33 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
 
+        int s = prices.size();
         int minP = prices[0];
         int max = 0;
 
-        for(int p : prices)
+        for(int i = 0; i < s; i++)
         {
-            minP = std::min(minP, p);
-            max = std::max(max, p - minP);
+            minP = std::min(minP, prices[i]);
+            max = std::max(max, prices[i] - minP);
         }
 
         return max;

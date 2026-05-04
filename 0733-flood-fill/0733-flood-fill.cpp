e9d971d7fddca6fc95e5b2cff3e9e42f9557692e
JaeHo Jeong <124887853+Jaero0@users.noreply.github.com>
2026-05-04 13:04:06 +0200

    Time: 4 ms (6.06%), Space: 19.4 MB (8.63%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    const int moveX[4] {-1,1,0,0};
+    const int moveY[4] {0,0,-1,1};
+
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+
+        int maxX = image.size();
+        int maxY = image[0].size();
+
+        bool visited[51][51] = {false};
+        std::queue<pair<int,int>> q;
+        int startColor = image[sr][sc];
+
+        q.push(make_pair(sr,sc));
+        visited[sr][sc] = true;
+
+        while(q.size() > 0)
+        {
+            auto srsc = q.front();
+            q.pop();
+
+            int x = srsc.first;
+            int y = srsc.second;
+            image[x][y] = color;
+
+            for(int i = 0; i < 4; i++)
+            {
+                int mx = x + moveX[i];
+                int my = y + moveY[i];
+
+                if (mx < 0 || mx >= maxX || my < 0 || my >= maxY) continue;
+                if (visited[mx][my] == true) continue;
+                if (image[mx][my] != startColor) continue;
+
+                visited[mx][my] = true;
+                q.push(make_pair(mx,my));
+            }
+        }
+
+        return image;
+    }
+};

bbfa294f250165fbabc62b8e8a097e604d745fda
JaeHo Jeong <124887853+Jaero0@users.noreply.github.com>
2026-05-04 13:13:21 +0200

    Time: 11 ms (2.15%), Space: 19.5 MB (8.63%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -8,10 +8,12 @@ public:
         int maxX = image.size();
         int maxY = image[0].size();
 
+        bool visited[51][51] = {false};
         std::queue<pair<int,int>> q;
         int startColor = image[sr][sc];
 
         q.push(make_pair(sr,sc));
+        visited[sr][sc] = true;
 
         while(q.size() > 0)
         {
@@ -29,8 +31,10 @@ public:
 
                 if (mx < 0 || mx >= maxX || 
                 my < 0 || my >= maxY || 
+                visited[mx][my] == true || 
                 image[mx][my] != startColor) continue;
 
+                visited[mx][my] = true;
                 q.push(make_pair(mx,my));
             }
         }

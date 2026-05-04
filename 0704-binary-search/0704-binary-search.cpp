8a6ac9b5d9a663bb0c3bebb1b5942aca43c1c278
JaeHo Jeong <124887853+Jaero0@users.noreply.github.com>
2026-05-04 12:31:40 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -5,14 +5,14 @@ public:
         int right = nums.size() - 1;
 
         while (left <= right) {
-            int mid = left + (right - left) / 2;
+            int mid = left + (right - left) / 2; 
 
             if (nums[mid] == target) {
                 return mid;
-            }
+            } 
             else if (nums[mid] < target) {
                 left = mid + 1;
-            }
+            } 
             else {
                 right = mid - 1;
             }

233647466115c6fbb4e5671a20319370aae0ea00
JaeHo Jeong <124887853+Jaero0@users.noreply.github.com>
2026-05-05 10:22:46 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -20,15 +20,7 @@ public:
 
         int left = height(node->left);
 
-        if (left == -1) {
-            return -1;
-        }
-
         int right = height(node->right);
-
-        if (right == -1) {
-            return -1;
-        }
 
         if (abs(left - right) > 1) {
             return -1;

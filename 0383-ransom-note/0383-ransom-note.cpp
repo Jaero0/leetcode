4b4641d2a0650bdd8e44fb401d0f4a052e0675d8
JaeHo Jeong <124887853+Jaero0@users.noreply.github.com>
2026-05-07 11:14:43 +0200

    Time: 3 ms (54.14%), Space: 11.7 MB (88.7%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -2,9 +2,24 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
         
+        queue<char> q;
+
         sort(ransomNote.begin(), ransomNote.end());
         sort(magazine.begin(), magazine.end());
 
-        return magazine.contains(ransomNote);
+        for(char c : ransomNote)
+        {
+            q.push(c);
+        }
+
+        for(char c : magazine)
+        {
+            if(q.front() == c)
+            {
+                q.pop();
+            }
+        }
+
+        return q.empty();
     }
 };

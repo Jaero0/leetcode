bf5495cf00fbfc804dc5e9c15c8938d0152a49b7
JaeHo Jeong <124887853+Jaero0@users.noreply.github.com>
2026-05-11 20:18:39 +0200

    Time: 55 ms (31.42%), Space: 90.9 MB (27.34%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+        unordered_map<int, int> m;
+
+        for(int i : nums)
+        {
+            if(m[i] > 0) return true;
+
+            m[i] += 1;
+        }
+
+        return false;
+    }
+};

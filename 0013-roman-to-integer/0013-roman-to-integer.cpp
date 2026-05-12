45fecd3f84d01ce938427eff92b4b3886ccc123e
JaeHo Jeong <124887853+Jaero0@users.noreply.github.com>
2026-05-12 11:02:13 +0200

    Time: 3 ms (76.8%), Space: 10.1 MB (78.93%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,45 +1,46 @@
 class Solution {
 public:
-    int romanToInt(string s) {
-        
-        map<string, int> m = {
-            {"M", 1000}, {"CM", 900}, {"D", 500}, {"CD", 400},
-            {"C", 100}, {"XC", 90}, {"L", 50}, {"XL", 40},
-            {"X", 10}, {"IX", 9}, {"V", 5}, {"IV", 4}, {"I", 1}
-        };
-
-        int sum = 0;
-        
-        for(int i = 0; i < s.size(); i++) 
-        {
-
-            string t(1, s[i]); 
-
-            if(i == s.size() - 1) 
-            {
-                sum += m[t];
-                continue;
-            }
-
-            if(t == "C" && (s[i+1] == 'M' || s[i+1] == 'D')) 
-            {
-                t += s[i + 1];
-                i += 1;
-            }
-            else if(t == "X" && (s[i+1] == 'L' || s[i+1] == 'C'))
-            {
-                t += s[i + 1];
-                i += 1;
-            }
-            else if(t == "I" && (s[i+1] == 'V' || s[i+1] == 'X')) 
-            { 
-                t += s[i + 1];
-                i += 1;
-            }
-
-            sum += m[t];
+    int getValue(char c) 
+    {
+        switch (c) {
+        case 'I':
+            return 1;
+        case 'V':
+            return 5;
+        case 'X':
+            return 10;
+        case 'L':
+            return 50;
+        case 'C':
+            return 100;
+        case 'D':
+            return 500;
+        case 'M':
+            return 1000;
+        default:
+            return 0;
         }
+    }
+    
+    int romanToInt(string s) {
+        int sum = 0;
+        int size = s.size();
 
+        for (int i = 0; i < size; i++) 
+        {
+            int current = getValue(s[i]);
+
+            if (i < size - 1) {
+                int next = getValue(s[i + 1]);
+
+                if (current < next) {
+                    sum += (next - current);
+                    i++; // 두 글자를 처리했으므로 인덱스 점프
+                    continue;
+                }
+            }
+            sum += current;
+        }
         return sum;
     }
 };

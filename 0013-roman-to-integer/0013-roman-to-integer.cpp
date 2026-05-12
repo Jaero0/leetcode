41d72b6f666c5f22ceb6c8385297527ba79531eb
JaeHo Jeong <124887853+Jaero0@users.noreply.github.com>
2026-05-12 10:59:06 +0200

    Time: 23 ms (6.01%), Space: 18.1 MB (6.7%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,61 +1,40 @@
 class Solution {
 public:
     int romanToInt(string s) {
-
+        
         map<string, int> m = {
-            {"M", 1000}, 
-            {"CM", 900},
-            {"D", 500}, 
-            {"CD", 400},
-            {"C", 100}, 
-            {"XC", 90},
-            {"L", 50},
-            {"XL", 40},
-            {"X", 10},   
-            {"IX", 9},
-            {"V", 5},   
-            {"IV", 4},
-            {"I", 1}
+            {"M", 1000}, {"CM", 900}, {"D", 500}, {"CD", 400},
+            {"C", 100}, {"XC", 90}, {"L", 50}, {"XL", 40},
+            {"X", 10}, {"IX", 9}, {"V", 5}, {"IV", 4}, {"I", 1}
         };
 
         int sum = 0;
         
-        for(int i = 0; i < s.size(); i++)
+        for(int i = 0; i < s.size(); i++) 
         {
-            string t = "";
-            t += s[i];
 
-            if(i == s.size() - 1)
+            string t(1, s[i]); 
+
+            if(i == s.size() - 1) 
             {
                 sum += m[t];
                 continue;
             }
 
-            if(t == "C" && (s[i+1] == 'M' || s[i+1] == 'D'))
+            if(t == "C" && (s[i+1] == 'M' || s[i+1] == 'D')) 
             {
                 t += s[i + 1];
                 i += 1;
-
-                sum += m[t];
-                continue;
             }
-
-            if(t == "X" && (s[i+1] == 'L' || s[i+1] == 'C'))
+            else if(t == "X" && (s[i+1] == 'L' || s[i+1] == 'C'))
             {
                 t += s[i + 1];
                 i += 1;
-
-                sum += m[t];
-                continue;
             }
-
-            if(t == "I" && (s[i+1] == 'V' || s[i+1] == 'X'))
-            {
+            else if(t == "I" && (s[i+1] == 'V' || s[i+1] == 'X')) 
+            { 
                 t += s[i + 1];
                 i += 1;
-
-                sum += m[t];
-                continue;
             }
 
             sum += m[t];

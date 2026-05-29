cafe95cab2960b0f72ab9b7f4f801624eceb53b7
JaeHo Jeong <124887853+Jaero0@users.noreply.github.com>
2026-05-29 16:58:57 +0200

    Time: 0 ms (100%), Space: 8.4 MB (10.37%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,17 +1,34 @@
+/*
+class Solution {
+public:
+    int hammingWeight(int n) 
+    {
+        int count = 0;
+        for (int i = 0; i < 32; i++) 
+        {
+            if ((n >> i) & 1) 
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+};
+*/
+
 class Solution {
 public:
     int hammingWeight(int n) 
     {
         string s;
-
         while(n != 0)
         {
-            int t = n % 2;
+        	int t = n%2;
             n /= 2;
             if(t == 0) continue;
-            s += to_string(t);
+            s += "1";
         }
-
+        
         return s.size();
     }
 };

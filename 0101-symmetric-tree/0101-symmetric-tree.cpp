10c0a12eeeedd68122a38ccf10cf9d2e6e775cc9
JaeHo Jeong <124887853+Jaero0@users.noreply.github.com>
2026-06-08 18:38:26 +0200

    Time: 0 ms (100%), Space: 18.4 MB (84.78%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -13,11 +13,19 @@ class Solution {
 public:
     bool same(TreeNode* l, TreeNode* r)
     {
-        if (!l && !r) return true;
-        if (!l || !r) return false;
-        if (l->val != r->val) return false;
-
-        return same(l->left, r->right) && same(l->right, r->left);
+	    //1. 두 노드가 모두 null일때
+        if(!l && !r) return true;
+    
+        //3번 종료조건과 바꾼...
+       //2. 두 노드 중 하나라도 null일때
+        if(!l || !r) return false;
+    
+        //2번 종료조건과 바꾼...
+        //3. 두 노드 모두 존재하고, 값이 다른경우
+        if(l->val != r->val) return false;
+    
+	    return same(l->left, r->right) &&
+    	       same(l->right, r->left);
     }
 
     bool isSymmetric(TreeNode* root) 

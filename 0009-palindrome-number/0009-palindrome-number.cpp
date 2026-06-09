78a41511784eb771b8aff716e2e5c8ee3afc54bd
JaeHo Jeong <124887853+Jaero0@users.noreply.github.com>
2026-06-09 16:28:52 +0200

    Time: 0 ms (100%), Space: 8.7 MB (37.49%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -2,17 +2,15 @@ class Solution {
 public:
     bool isPalindrome(int x) 
     {
-        if(x < 0) return false;
-        if(x % 10 == 0 && x != 0) return false;
+        int origin = x;
+        long reverse = 0;
 
-        int reverse = 0;
-
-        while(x > reverse)
+        while(x > 0)
         {
             reverse = reverse * 10 + x % 10;
             x /= 10;
         }
 
-        return reverse == x || reverse / 10 == x;
+        return origin == reverse;
     }
 };
